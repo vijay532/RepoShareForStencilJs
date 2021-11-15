67905d31c2a1ee76e53c5fb9468d466029f589b2
vijay532 <sahaniv66@gmail.com>
2021-11-15 14:42:18 +0530

    filtering done

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -1,5 +1,4 @@
-import { Component, Prop, h, State, Event, EventEmitter } from '@stencil/core';
-import { format } from '../../utils/utils';
+import { Component, h, State } from '@stencil/core';
 
 @Component({
   tag: 'my-component',
@@ -7,14 +6,11 @@ import { format } from '../../utils/utils';
   shadow: true,
 })
 export class MyComponent {
-  
-  @State() search: String ;
+  @State() search: string;
 
-  @State() items: { active:number, deaths:number, confirmed:number,statecode:string }[];
+  @State() items: { active: number; deaths: number; confirmed: number; statecode: string }[];
 
   componentWillLoad() {
-    
-
     return fetch('https://data.covid19india.org/data.json')
       .then(res => res.json())
       .then(data => {
@@ -23,41 +19,44 @@ export class MyComponent {
       .catch(err => console.log(err));
   }
 
-  updateVal=(e)=>{
-    this.search=(e.target as HTMLInputElement).value;
+  updateVal = e => {
+    this.search = (e.target as HTMLInputElement).value;
     console.log(this.search);
-  }
+  };
   render() {
-    // console.log(this.items);
+   
+    const newItem = this.search
+      ? this.items.filter(item => {
+          return item.statecode === this.search;
+        })
+      : this.items;
+
     return (
       <div class="app">
         <div class="container">
-          <div>
-            <input class="search" type="search" onChange={(e)=>this.updateVal(e)} placeholder="Search State"/>
-          </div>
+          <input class="searchbar mb-15" type="search" onChange={e => this.updateVal(e)} placeholder="Search State....." />
+
           <div class="row">
             {
-              // this.items!=null && typeof this.items!='undefined' && this.items.length > 0 && 
-              this.items
-                .filter((search)=>{
-                  console.log("hii"+this.search);
-                  return this.items.statecode.includes(this.search)
-                })
-                .map((content) =>
-                  <div class="col-md-3 col-sm-6 col-4 mb-15">
-                    <div class="card-container">
-                      <p> Confirmed: {content.confirmed}</p>
-                      <p> Active: {content.active} </p>
-                      <p> Deaths : {content.deaths} </p>
-                      <p> StateCode: <strong>{content.statecode}</strong> </p>
-                    </div>
+              // this.items!=null && typeof this.items!='undefined' && this.items.length > 0 &&
+
+              newItem.map(content => (
+                <div class="col-md-3 col-sm-6 col-4 mb-15">
+                  <div class="card-container">
+                    <p> Confirmed: {content.confirmed}</p>
+                    <p> Active: {content.active} </p>
+                    <p> Deaths : {content.deaths} </p>
+                    <p>
+                      {' '}
+                      StateCode: <strong>{content.statecode}</strong>{' '}
+                    </p>
                   </div>
-                )
+                </div>
+              ))
             }
           </div>
         </div>
       </div>
-     
     );
   }
 }
